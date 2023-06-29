049c571b95824e5d4a103da84a24be1a3c6db737
sourav <soutavr5@gmail.com>
2023-06-29 20:14:40 +0530

    user

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('<int:quiz_id>/', views.quiz_detail, name='quiz_detail'),
     path('<int:quiz_id>/submit/', views.submit_answer, name='submit_answer'),
     path('<int:quiz_id>/certificate/', views.generate_certificate, name='generate_certificate'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- quizapp/views.py
+++ quizapp/views.py
@@ -1,16 +1,21 @@
-
-# quiz/views.py
-# quiz/views.py
-from django.shortcuts import render, get_object_or_404
-from .models import Quiz, Question,Answer
+from django.shortcuts import render, get_object_or_404,redirect
+from .models import Quiz, Question, Answer
 from django.http import HttpResponse
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Image
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .forms import LoginForm, RegistrationForm
 
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
     questions = quiz.question_set.all()
     context = {'quiz': quiz, 'questions': questions}
     return render(request, 'quiz/quiz_detail.html', context)
+
 
 def submit_answer(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
@@ -19,7 +24,11 @@ def submit_answer(request, quiz_id):
 
     for question in questions:
         selected_answer_id = request.POST.get(f'answer_{question.id}')
-        selected_answer = get_object_or_404(Answer, id=selected_answer_id)
+        try:
+            selected_answer = question.answer_set.get(id=selected_answer_id)
+        except Answer.DoesNotExist:
+            return HttpResponse("Invalid answer selected.")
+        
         if selected_answer.is_correct:
             total_marks += 1
 
@@ -31,6 +40,7 @@ def quiz_list(request):
     return render(request, 'quiz/quiz_list.html', {'quizzes': quizzes})
 
 # Other view functions...
+
 def generate_certificate(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
     # Retrieve user information or any other relevant data for the certificate
@@ -38,7 +48,7 @@ def generate_certificate(request, quiz_id):
 
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="certificate.pdf"'
-
+    
     # Generate the PDF
     p = canvas.Canvas(response)
     
@@ -54,4 +64,37 @@ def generate_certificate(request, quiz_id):
 
     p.showPage()
     p.save()
-    return response
+    return 
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('quiz_list')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+    return render(request, 'quiz/login.html', {'form': form})
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful. Please log in.')
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'quiz/register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('quiz_list')
